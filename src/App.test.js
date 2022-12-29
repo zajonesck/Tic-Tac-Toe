e4becbd82bc8d06e53665e61fc3a8d570680424f
Zack Jones <zajonesck83@gmail.com>
2022-12-28 21:22:47 -0600

    fixed test and deleted some boiler plate

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders my github link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My GitHub/i);
   expect(linkElement).toBeInTheDocument();
 });
