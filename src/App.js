833188aabeb8054f36f561a9cef67e971082e41f
Zack Jones <zajonesck83@gmail.com>
2023-01-02 22:55:49 -0600

    added board and some logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import "./App.css";
+import { useState } from "react";
+import Square from "./Components/square";
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
+
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((val, idx) => {
+        if (idx == square && val == "") {
+          return player;
+        }
+        return val;
+      })
+    );
+  };
   return (
     <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+        </div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
       <header className="App-header">
         <p>TIC TAC TOE</p>
         <a
