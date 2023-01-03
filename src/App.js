38f3f70f46deafbfebd70771a2b747ddb6d20d17
Zack Jones <zajonesck83@gmail.com>
2023-01-03 08:57:57 -0600

    finished all squares and added logic to switch between players

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ function App() {
         return val;
       })
     );
+
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
   };
   return (
     <div className="App">
@@ -39,8 +45,46 @@ function App() {
             }}
           />
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Square
+            val={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3);
+            }}
+          />
+          <Square
+            val={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4);
+            }}
+          />
+          <Square
+            val={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6);
+            }}
+          />
+          <Square
+            val={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7);
+            }}
+          />
+          <Square
+            val={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8);
+            }}
+          />
+        </div>
       </div>
       <header className="App-header">
         <p>TIC TAC TOE</p>
