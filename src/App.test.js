a436f94b6c1a7c8d8f12ab9f646b7e7fc398fe6b
Zack Jones <zajonesck83@gmail.com>
2023-01-10 21:38:45 -0600

    passed link test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
 import React from "react";
-
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders my github link", () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/My GitHub/i);
-  // expect(linkElement).toBeInTheDocument();
-  expect(true).toBe(true);
+  render(<App />);
+  const linkElement = screen.getByText(/My GitHub/i);
+  expect(linkElement).toBeInTheDocument();
 });
